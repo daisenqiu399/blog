3c18c8ef67d5bbabb3dc8640e074beb7ee9bb759
ZbWeR <1641064392@qq.com>
2023-09-14 16:47:10 +0800

    ✨ feat: 小徽章组件

---
+++ docs/.vitepress/components/Badge.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="badge text-slate-100" :class="getColor">
+    <slot>UESTCer</slot>
+  </span>
+</template>
+
+<script setup>
+import { computed } from "vue";
+const { color } = defineProps(["color"]);
+console.log(color);
+const getColor = computed(() => {
+  if (color) return `bg-${color}-500`;
+  return "bg-gray-500";
+});
+</script>
+
+<style scoped>
+.badge {
+  display: inline-block;
+  padding: 0 10px;
+  line-height: 22px;
+  font-size: 12px;
+  border-radius: 12px;
+}
+</style>
