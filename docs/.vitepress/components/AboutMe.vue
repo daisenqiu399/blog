af527eaa697a6c3bcdcb85072c599b7c95c7ca4c
ZbWeR <1641064392@qq.com>
2023-09-14 23:11:49 +0800

    ✨ feat: About页面头像&徽章

---
+++ docs/.vitepress/components/AboutMe.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="px-6 mt-2 md:px-12 lg:px-16">
+    <div class="flex flex-col items-center justify-center max-w-6xl mx-auto">
+      <!-- 头像与简介 -->
+      <div class="relative w-full mx-auto">
+        <!-- 头像 -->
+        <div class="relative py-2 h-52">
+          <div
+            class="absolute w-48 h-48 mx-auto rounded-full shadow-[0px_0px_3px#147eae] dark:shadow-[0px_0px_3px#9ba0a9] top-1/2 avatar-ping left-1/2"
+          ></div>
+          <img
+            class="block w-48 h-48 mx-auto rounded-full"
+            src="/assets/avatar.jpg"
+            alt="avatar"
+          />
+        </div>
+        <!-- Right-tags -->
+        <p
+          style="top: 32%"
+          class="tag-bounce hidden sm:block sm:left-[69%] md:left-[66%] lg:left-[63%] xl:left-[60%] absolute px-3 py-0.5 text-sm font-bold rounded-full shadow-[0px_0px_1px_1px#eee]"
+        >
+          🎵 二进制吟游诗人
+        </p>
+        <p
+          style="top: 50%"
+          class="tag-bounce hidden sm:block sm:left-[69%] md:left-[66%] lg:left-[63%] xl:left-[60%] absolute px-3 py-0.5 text-sm font-bold rounded-full shadow-[0px_0px_1px_1px#eee]"
+        >
+          绿茵场最佳气氛组⚽
+        </p>
+        <!-- Left-tags -->
+        <p
+          style="top: 32%"
+          class="tag-bounce hidden sm:block sm:left-[8%] md:left-[13%] lg:left-[21%] xl:left-[27%] absolute px-3 py-0.5 text-sm font-bold rounded-full shadow-[0px_0px_1px_1px#eee]"
+        >
+          🌙 倒头就睡懒羊羊
+        </p>
+        <p
+          style="top: 50%"
+          class="tag-bounce hidden sm:block sm:left-[8%] md:left-[13%] lg:left-[21%] xl:left-[27%] absolute px-3 py-0.5 text-sm font-bold rounded-full shadow-[0px_0px_1px_1px#eee]"
+        >
+          宇宙无敌暴龙战士🔥
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.avatar-ping {
+  animation: avatarPing 2s ease-in infinite;
+}
+@keyframes avatarPing {
+  0%,
+  100% {
+    transform: translateX(-50%) translateY(-50%) scale(1.05);
+  }
+  50% {
+    transform: translateX(-50%) translateY(-50%) scale(1.1) 9;
+  }
+}
+.tag-bounce {
+  animation: tagBounce 1.5s ease-in infinite;
+}
+@keyframes tagBounce {
+  0%,
+  100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-10%);
+  }
+}
+</style>
---
+++ docs/src/AboutMe.md
@@ -0,0 +1,12 @@
+---
+layout: page
+---
+
+<script setup>
+
+import AboutMe from '../.vitepress/components/AboutMe.vue'
+
+
+</script>
+
+<AboutMe/>
