8eb5b6525cc887e6359c08f260ea3cd10f367c01
ZbWeR <1641064392@qq.com>
2023-09-16 21:45:48 +0800

    ✨ feat: 自定义更新时间组件

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,9 @@
 import DefaultTheme from "vitepress/theme";
 import "./tailwind.css";
 import "./var.css";
+import MyLayout from "../components/MyLayout.vue";
 
-export default DefaultTheme;
+export default {
+  ...DefaultTheme,
+  Layout: MyLayout,
+};
