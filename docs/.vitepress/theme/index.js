b56fcae1c909b7744f65a3862522a35683eb7296
ZbWeR <1641064392@qq.com>
2023-09-26 18:55:30 +0800

    🌈 style: 自定义文档样式 - 紫色主题

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,7 @@
 import DefaultTheme from "vitepress/theme";
 import "./tailwind.css";
 import "./var.css";
+import "./artical.css";
 // import MyLayout from "../components/MyLayout.vue";
 
 export default {
