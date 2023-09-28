edf88e8b390cc856d24f8942b4986206fffa9400
ZbWeR <1641064392@qq.com>
2023-09-28 11:21:37 +0800

    ✨ feat: 链接卡片支持自定义 icon

--- docs/.vitepress/components/LinkCard.vue
+++ docs/.vitepress/components/LinkCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div
     @click="openLink"
-    class="link-card my-3 p-4 transition-all duration-300 rounded-lg shadow-md cursor-pointer hover:bg-sky-100/80 dark:shadow-sky-900/80 hover:scale-[0.99] shadow-sky-400/30 dark:bg-sky-950 dark:hover:bg-sky-950/80 bg-sky-100/50 group"
+    class="link-card my-3 p-4 transition-all duration-300 rounded-lg shadow-sm cursor-pointer hover:bg-blue-100/80 dark:shadow-sky-900/80 hover:scale-[0.99] shadow-blue-400/60 dark:bg-sky-950 dark:hover:bg-sky-950/80 bg-blue-200/30 group"
   >
-    <header class="text-xl">🐳 {{ desc }}</header>
+    <header class="font-bold">{{ icon || "🐳" }} {{ desc }}</header>
     <footer
-      class="mt-1 transition-all duration-300 text-slate-500 dark:text-slate-400 dark:group-hover:text-sky-400 group-hover:text-sky-500 line-clamp-1"
+      class="mt-1 transition-all duration-300 text-slate-500 dark:text-slate-400 dark:group-hover:text-sky-400 line-clamp-1"
     >
       {{ link }}
     </footer>
@@ -13,8 +13,7 @@
 </template>
 
 <script setup>
-const { desc, link } = defineProps(["desc", "link"]);
-
+const { desc, link, icon } = defineProps(["desc", "link", "icon"]);
 function openLink() {
   window.open(link, "_blank");
 }
