f30fb2d60656b5243fe59f15c39e44e5594f749e
ZbWeR <1641064392@qq.com>
2024-09-17 15:10:47 +0800

    ✨ feat: 优化页脚显示逻辑

--- docs/.vitepress/components/Footer.vue
+++ docs/.vitepress/components/Footer.vue
@@ -1,18 +1,34 @@
 <template>
   <footer
-    class="relative px-6 pt-8 pb-4 text-center border-t dark:border-zinc-800 sm:pt-8 text-black/40 dark:text-zinc-300/40"
+    v-if="showFooter"
+    class="relative border-t px-6 pb-4 pt-8 text-center text-black/40 dark:border-zinc-800 dark:text-zinc-300/40 sm:pt-8"
   >
     <p class="text-sm font-medium">{{ footer.copyright }}</p>
     <p class="text-sm font-medium">
       Theme |
-      <a href="https://github.com/ZbWeR/easy-vitepress-blog">easy-vitepress-blog</a>
+      <LinkText
+        text="Easy-Vitepress-Blog"
+        link="https://github.com/ZbWeR/easy-vitepress-blog"
+        :icon="ArrowTopRightOnSquareIcon"
+      />
     </p>
   </footer>
 </template>
 
 <script setup lang="ts">
-import { useData } from "vitepress";
+import { useData, useRoute } from 'vitepress'
+import LinkText from './LinkText.vue'
+import ArrowTopRightOnSquareIcon from '@heroicons/vue/16/solid/esm/ArrowTopRightOnSquareIcon'
+import { computed } from 'vue';
+import { useSidebar } from 'vitepress/theme';
 
-const { theme } = useData();
-const { footer } = theme.value;
+const { theme, frontmatter } = useData()
+const { footer } = theme.value
+
+const showFooter = computed(()=>{
+  const footer:boolean = frontmatter.value.footer ?? true;
+  const { hasSidebar } = useSidebar();
+  // 当侧边栏可见时，不显示页脚
+  return footer && !hasSidebar.value;
+})
 </script>
