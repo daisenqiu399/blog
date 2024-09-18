1ba30fde216a75e5dc39167f83e7fc7648c38686
ZbWeR <1641064392@qq.com>
2024-09-18 17:23:23 +0800

    ✨ feat: 首页全新升级！

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./docs/.vitepress/**/*.{vue,js}", "./docs/**/*.md"],
+  content: ['./docs/.vitepress/**/*.{vue,js}', './docs/**/*.md'],
   theme: {
     extend: {
       colors: {
-        VPLight: "#3451b2",
-        VPDark: "#a8b1ff",
+        VPLight: '#3451b2',
+        VPDark: '#a8b1ff'
       },
-    },
+      animation: {
+        'scale-in-center': 'scale-in-center 0.8s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both'
+      },
+      keyframes: {
+        'scale-in-center': {
+          '0%': {
+            transform: 'scale(0)',
+            opacity: '1'
+          },
+          to: {
+            transform: 'scale(1)',
+            opacity: '1'
+          }
+        }
+      }
+    }
   },
   plugins: [],
-  darkMode: "class",
-};
+  darkMode: 'class'
+}
